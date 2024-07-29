4a5809488f741e53d72561f18162742b108ebc49
Asim-108 <asim.rahman321@gmail.com>
2024-07-28 21:00:57 -0400

    added test functionality to main page, created stat variable and assigned on build button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+    const[stat, setStat] = useState<undefined | string>(undefined);
+    console.log(stat);
+
   return (
  
 <body>
     <div className="navBar">
         <img src={require("./assets/elden logo.png")} alt="elden logo" />
-        <button className="navButton">
-            <img src={require("./assets/nav logo.png")} alt="nav compass" />
-        </button>
+        <a href="#buildSelect">
+            <button className="navButton">
+                    <img src={require("./assets/nav logo.png")} alt="nav compass" />
+            </button>
+        </a>
         <button className="navButton">Weapon</button>
         <button className="navButton">Equipment</button>
     </div>
@@ -20,16 +27,46 @@ function App() {
         <p>By: Asim</p>
     </div>
 
-    <div>
+    <div id='buildSelect'>
         <h1>Choose Build</h1>
-        <button className="buildButton">Strength</button>
-        <button className="buildButton">Dexterity</button>
-        <button className="buildButton">Intelligence</button>
-        <button className="buildButton">Faith</button>
-        <button className="buildButton">Arcane</button>
-        <button className="buildButton">Surprise Me!</button>
+        <button className="buildButton" onClick={()=>setStat('str')}>Strength</button>
+        <button className="buildButton" onClick={()=>setStat('dex')}>Dexterity</button>
+        <button className="buildButton" onClick={()=>setStat('int')}>Intelligence</button>
+        <button className="buildButton" onClick={()=>setStat('fai')}>Faith</button>
+        <button className="buildButton" onClick={()=>setStat('arc')}>Arcane</button>
+        <button className="buildButton" onClick={()=>setStat('rand')}>Surprise Me!</button>
     </div>
-    
+    <br />
+    <br />
+    <br />
+
+    {
+        stat && (
+                <>   
+                    <div>
+                        <h1>Choose Weapon</h1>
+                        <button>TEST1</button>
+                        <button>TEST2</button>
+                        <button>TEST3</button>
+                    </div>
+                
+                    <div>
+                        <h1>Your Build</h1>
+                        <div id='flex'>
+                            <div className='textBox' id='box'>
+                            </div>
+                            <div className='textBox' id='box'>
+                            </div>
+                            <div className='textBox' id='box'>
+                            </div>     
+                        </div>
+                        
+                    </div>
+            </>
+        )
+        
+    }
+
 </body>
   );
 }
